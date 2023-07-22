ab7d4acd50f21e147b5bed96b9d7b8ed962f8605
Masato Ito <oimasato@gmail.com>
2023-07-22 12:45:57 +0900

    Cleaning up Core Data

---
+++ Tag-CoreDataHelpers.swift
@@ -0,0 +1,46 @@
+//
+//  Tag-CoreDataHelpers.swift
+//  Portfolio
+//
+//  Created by イトマサ on 2023/07/22.
+//
+
+import Foundation
+
+extension Tag {
+    static var example: Tag {
+        let controller = DataController(inMemory: true)
+        let viewContext = controller.container.viewContext
+
+        let tag = Tag(context: viewContext)
+        tag.id = UUID()
+        tag.name = "Example Tag"
+        return tag
+    }
+
+    var tagID: UUID {
+        id ?? UUID()
+    }
+
+    var tagName: String {
+        name ?? ""
+    }
+    
+    var tagActiveIssues: [Issue] {
+        let result = issues?.allObjects as? [Issue] ?? []
+        return result.filter { $0.completed == false }
+    }
+}
+
+extension Tag: Comparable {
+    public static func <(lhs: Tag, rhs: Tag) -> Bool {
+        let left = lhs.tagName.localizedLowercase
+        let right = rhs.tagName.localizedLowercase
+
+        if left == right {
+            return lhs.tagID.uuidString < rhs.tagID.uuidString
+        } else {
+            return left < right
+        }
+    }
+}
