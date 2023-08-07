cae87acceef8a939d0825a441cb9390e962f37a0
Masato Ito <oimasato@gmail.com>
2023-08-07 10:43:42 +0900

    Testing bundled data

---
+++ PortfolioTests/AssetTests.swift
@@ -0,0 +1,25 @@
+//
+//  AssetTests.swift
+//  PortfolioTests
+//
+//  Created by イトマサ on 2023/08/07.
+//
+
+import XCTest
+@testable import Portfolio
+
+class AssetTests: XCTestCase {
+    
+    func testColorsExist() {
+        let allColors = ["Dark Blue", "Dark Gray", "Gold", "Gray", "Green",
+                         "Light Blue", "Midnight", "Orange", "Pink", "Purple", "Red", "Teal"]
+
+        for color in allColors {
+            XCTAssertNotNil(UIColor(named: color), "Failed to load color '\(color)' from asset catalog.")
+        }
+    }
+
+    func testAwardsLoadCorrectly() {
+        XCTAssertTrue(Award.allAwards.isEmpty == false, "Failed to load awards from JSON.")
+    }
+}
---
+++ PortfolioTests/PortfolioTests.swift
@@ -0,0 +1,20 @@
+//
+//  PortfolioTests.swift
+//  PortfolioTests
+//
+//  Created by イトマサ on 2023/08/07.
+//
+
+import CoreData
+import XCTest
+@testable import Portfolio
+
+class BaseTestCase: XCTestCase {
+    var dataController: DataController!
+    var managedObjectContext: NSManagedObjectContext!
+
+    override func setUpWithError() throws {
+        dataController = DataController(inMemory: true)
+        managedObjectContext = dataController.container.viewContext
+    }
+}
