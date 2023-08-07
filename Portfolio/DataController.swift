7581f3b0781e912860ab4a3f8190088fab87d549
Masato Ito <oimasato@gmail.com>
2023-08-07 13:30:50 +0900

    Testing Core Data

--- Portfolio/DataController.swift
+++ Portfolio/DataController.swift
@@ -13,6 +13,19 @@ class DataController: ObservableObject {
         dataController.createSampleData()
         return dataController
     }()
+    
+    static let model: NSManagedObjectModel = {
+        guard let url = Bundle.main.url(forResource: "Main", withExtension: "momd") else {
+            fatalError("Failed to locate model file.")
+        }
+
+        guard let managedObjectModel = NSManagedObjectModel(contentsOf: url) else {
+            fatalError("Failed to load model file.")
+        }
+
+        return managedObjectModel
+    }()
+    
     let container: NSPersistentCloudKitContainer
     
     @Published var selectedFilter: Filter? = Filter.all
@@ -42,7 +55,7 @@ class DataController: ObservableObject {
     }
     
     init(inMemory: Bool = false) {
-        container = NSPersistentCloudKitContainer(name: "Main")
+        container = NSPersistentCloudKitContainer(name: "Main", managedObjectModel: Self.model)
 
         if inMemory {
             container.persistentStoreDescriptions.first?.url = URL(filePath: "/dev/null")
