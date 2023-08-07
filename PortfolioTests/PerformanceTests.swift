a23ba833e908505f19bea8f06edecfe14d4919f5
Masato Ito <oimasato@gmail.com>
2023-08-07 18:19:43 +0900

    Measuring performance

--- PortfolioTests/PerformanceTests.swift
+++ PortfolioTests/PerformanceTests.swift
@@ -6,8 +6,21 @@
 //
 
 import XCTest
+@testable import Portfolio
 
 final class PerformanceTests: BaseTestCase {
+    // Create a significant amount of test data
+    func testAwardCalculationPerformance() {
+        for _ in 1...100 {
+            dataController.createSampleData()
+        }
 
-    
+        // Simulate lots of awards to check
+        let awards = Array(repeating: Award.allAwards, count: 25).joined()
+        XCTAssertEqual(awards.count, 500, "This checks the awards count is constant. Change this if you add awards.")
+
+        measure {
+            _ = awards.filter(dataController.hasEarned)
+        }
+    }
 }
